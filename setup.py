32b57a481565a8968178ab16bb11a34edf4c001f
Godefroid Chapelle <gotcha@bubblenet.be>
2014-10-16 10:28:39 +0200

    remove dependency on ecreall.helpers.testing

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
     ],
     extras_require={
         'test': [
-            'ecreall.helpers.testing',
             'plone.app.testing',
             'plone.app.robotframework',
         ],
