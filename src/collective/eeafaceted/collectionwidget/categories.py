5729e9538654d8b01b4bed85ced9a215701ef94e
Gauthier Bastien <g.bastien@imio.be>
2015-04-08 19:36:16 +0200

    Only add an empty category if necessary, so when collections are defined at the root

--- src/collective/eeafaceted/collectionwidget/categories.py
+++ src/collective/eeafaceted/collectionwidget/categories.py
@@ -9,11 +9,20 @@ class CategoriesFromFolder(object):
     @property
     def values(self):
         catalog = getToolByName(self.context, 'portal_catalog')
-        brains = catalog(
+        # add an empty category if some collection are defined at the root level
+        result = []
+        collections = catalog(
+            path=dict(query='/'.join(self.context.getPhysicalPath()), depth=1),
+            portal_type='Collection',
+            sort_on="getObjPositionInParent"
+        )
+        if collections:
+            result.append(('', u''))
+
+        folders = catalog(
             path=dict(query='/'.join(self.context.getPhysicalPath()), depth=1),
             portal_type='Folder',
             sort_on="getObjPositionInParent"
         )
-        result = [('', u''), ]
-        result = result + [(brain.getId, brain.Title) for brain in brains]
+        result = result + [(brain.getId, brain.Title) for brain in folders]
         return result
