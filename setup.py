b0f734e782e0c8f164b8180ba518346844555a40
Gauthier Bastien <g.bastien@imio.be>
2015-05-04 09:43:04 +0200

    Get rid of grok

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'five.grok',
         'plone.api',
         'setuptools',
         'eea.facetednavigation',
