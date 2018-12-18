93e8f69a4e33a8f15dc12e23ee3177cbad0521f4
Gauthier Bastien <g.bastien@imio.be>
2018-12-18 16:00:25 +0100

    Back to development: 1.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (
 
 setup(
     name='collective.eeafaceted.collectionwidget',
-    version='1.5',
+    version='1.6.dev0',
     description=(
         "eea.facetednavigation widget that enables selecting "
         "a collection (among several) as base filter"),
