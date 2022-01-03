e91c2c989ef76b6c7b27a78baf079025815c9a51
Gauthier Bastien <gauthier.bastien@imio.be>
2022-01-03 10:13:34 +0100

    Completed classifiers

--- setup.py
+++ setup.py
@@ -17,11 +17,13 @@ setup(
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        "Development Status :: 6 - Mature",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
     ],
